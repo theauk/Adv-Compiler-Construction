50e1f38dbb57a7c8590750826bd9fcabd86a2149
Thea Kjeldsmark <tkjeldsmark@gmail.com>
2024-01-14 19:57:25 -0800

    Update tests

--- warmup-1/README.md
+++ warmup-1/README.md
@@ -4,3 +4,6 @@ Interpreter that parses arithmetic integer expressions.
 
 ## How to run
 Run the command `python run.py`, and enter the input. Enter `exit` to exit.
+
+## Tests
+To run the tests, run the command `python test_interpreter.py` 
--- warmup-1/test_interpreter.py
+++ warmup-1/test_interpreter.py
@@ -6,8 +6,8 @@ import interpreter
 class TestInterpreter(unittest.TestCase):
 
     def test_parse(self):
-        result = interpreter.main("2+3.5*4.")
-        self.assertEqual(result, ["5", "20"])
+        result = interpreter.main("2+3.5*4.9-10.788.")
+        self.assertEqual(result, ["5", "20", "-1", "788"])
 
     def test_space(self):
         result = interpreter.main("2 +  3. 5*   4 .")
@@ -16,6 +16,12 @@ class TestInterpreter(unittest.TestCase):
     def test_paren(self):
         result = interpreter.main("1+2-(4*2)+(3+3).")
         self.assertEqual(result, ["1"])
+
+        result = interpreter.main("1+(4*2)*(3+3)+5.")
+        self.assertEqual(result, ["54"])
+
+        result = interpreter.main("(07-(6*02)-95)+77.")
+        self.assertEqual(result, ["-23"])
 
     def test_exceptions(self):
         result = interpreter.main("2+2a.")
